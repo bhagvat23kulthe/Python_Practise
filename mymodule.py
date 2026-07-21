e64db0d1ffbd9d52ce5731c6f3ab754f2861c237
Bhagvat Kulthe <Bhagvat23kulthe@gmail.com>
2026-07-21 11:29:22 +0530

    first commit

---
+++ mymodule.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print("Hello", name)
+
+greet("Bhagvat")
+greet("Mihtun")
+greet("Aashish")
+
+x = "Teri maa ki choomt"
+
+
+person1 = {
+    "name" : "John",
+    "age" : 36,
+    "country" : "Norway"
+}
+
+p = ("Teri ma ki choomt")
+print(p)
